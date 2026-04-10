d14d930a2691b9cbe20188269496e8bada04386c
pr4veen44 <175582583+pr4veen44@users.noreply.github.com>
2026-04-10 23:36:19 +0530

    docs: update GitHub profile URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
